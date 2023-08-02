31743d47e7055152aced4e799a7e63d71caf8723
ILXYZ <ikigailabs@proton.me>
2023-08-02 15:36:58 +0200

    commit

--- templates/HomePage/index.tsx
+++ templates/HomePage/index.tsx
@@ -19,7 +19,7 @@ const HomePage = () => {
             <Collections />
             <Auctions items={auctions} />
             <Spotlight />
-            <Creativity />
+            // <Creativity />
             <Newsletter />
         </Layout>
     );
